b19d3292604349d38eeda7c7d6afea06d6c4a1f1
XtratoS <ultimate.nuker@yahoo.com>
2021-01-21 06:46:07 +0200

    Added bootstrap

--- would-you-rather/src/index.js
+++ would-you-rather/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import './bootstrap-5.css';
 
 ReactDOM.render(
     <App />,
