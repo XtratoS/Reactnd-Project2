ac6cefbb1098fe9cf44fd275f80a8ba3fa021b45
XtratoS <ultimate.nuker@yahoo.com>
2021-01-24 19:33:35 +0200

    Added actions to add a new user

--- would-you-rather/src/actions/users.js
+++ would-you-rather/src/actions/users.js
@@ -1,8 +1,31 @@
+import { hideLoading, showLoading } from "react-redux-loading";
+import { saveUser } from "../utils/api";
+import { formatUser } from "../utils/helpers";
+
 export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const ADD_USER = 'ADD_USER';
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function addUser(user) {
+    return {
+        type: ADD_USER,
+        user
+    }
+}
+
+export function handleAddUser(user) {
+    const formattedUser = formatUser(user);
+    return function(dispatch) {
+        dispatch(showLoading());
+        saveUser(formattedUser).then((addedUser) => {
+            dispatch(addUser(addedUser));
+            dispatch(hideLoading());
+        });
     }
 }
