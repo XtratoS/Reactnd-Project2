8042731a0e8109282a06ce94e82ab56fe9080464
XtratoS <ultimate.nuker@yahoo.com>
2021-01-24 19:34:31 +0200

    ADD_USER action handler in the users reducer

--- would-you-rather/src/reducers/users.js
+++ would-you-rather/src/reducers/users.js
@@ -1,9 +1,11 @@
-import { RECEIVE_USERS } from "../actions/users";
+import { RECEIVE_USERS, ADD_USER } from "../actions/users";
 
 export function users(state = {}, action) {
     switch(action.type) {
         case RECEIVE_USERS:
             return action.users;
+        case ADD_USER:
+            return {...state, ...action.user}
         default:
             return state;
     }
