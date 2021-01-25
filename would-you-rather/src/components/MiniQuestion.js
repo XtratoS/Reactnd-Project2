f98d5e41bd46dad19f38ada7b32bea8ec8bbaa1f
XtratoS <ultimate.nuker@yahoo.com>
2021-01-25 19:55:52 +0200

    Added linking to the question page

--- would-you-rather/src/components/MiniQuestion.js
+++ would-you-rather/src/components/MiniQuestion.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { timestampToText } from '../utils/helpers';
 
 class MiniQuestion extends Component {
     render() {
         const { question, author } = this.props;
         return (
-            <div className="question row m-2">
-                <div className="col-4 m-auto question-extradata">
-                    <div className="author">{author.name}</div>
-                    <div className="avatar">
-                        <img
-                            src={author.avatarURL}
-                            alt="avatar"
-                            className="avatar-image"
-                        />
+            <Link to={`questions/${question.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <div className="question row m-2 p-0">
+                    <div className="col-5 m-auto question-extradata">
+                        <div className="author">{author.name}</div>
+                        <div className="avatar">
+                            <img
+                                src={author.avatarURL}
+                                alt="avatar"
+                                className="avatar-image"
+                            />
+                        </div>
+                        <div className="date">{timestampToText(question.timestamp)}</div>
                     </div>
-                    <div className="date">{timestampToText(question.timestamp)}</div>
+                    <div className="col-7 m-auto">
+                        <div className="h5 m-0">Would You Rather?</div>
+                        <div className="hint">{question.hint || ''}</div>
+                    </div>
                 </div>
-                <div className="col-8 m-auto">
-                    <div className="h5 m-0">Would You Rather?</div>
-                    <div className="hint">{question.hint || ''}</div>
-                </div>
-            </div>
+            </Link>
         )
     }
 }
