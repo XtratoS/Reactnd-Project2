4b18563765e67b5c69256e684a3f3dcb374df756
XtratoS <ultimate.nuker@yahoo.com>
2021-01-25 15:13:49 +0200

    Added Leaderboard view

---
+++ would-you-rather/src/components/Leaderboard.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import LeaderboardRow from './LeaderboardRow'
+
+export class Leaderboard extends Component {
+    componentDidMount() {
+        console.log(this.props.users);
+    }
+
+    render() {
+        const { users } = this.props
+        return (
+            <div className="small-container my-4">
+                <div className="h3 m-3 text-center">Leaderboard</div>
+                {users.map((user) => (
+                    <LeaderboardRow
+                        key={user.id}
+                        user={user}
+                    />
+                ))}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    let usersPoints = [];
+    for (let userId in state.users) {
+        const user = state.users[userId]
+        const asked = user.questions.length;
+        const answered = Object.keys(user.answers).length;
+        let userPoints = {
+            id: userId,
+            name: user.name,
+            avatarURL: user.avatarURL,
+            asked,
+            answered,
+            total: asked + answered
+        }
+        usersPoints.push(userPoints);
+    }
+    usersPoints.sort((a, b) => b.total - a.total);
+    let pos = 1;
+    let lastP = 0;
+    usersPoints = usersPoints.map((user) => {
+        if (user.total < lastP) {
+            pos++;
+        }
+        lastP = user.total
+        return {
+            ...user, pos
+        }
+    })
+    return {users: usersPoints};
+}
+
+export default connect(mapStateToProps)(Leaderboard);
---
+++ would-you-rather/src/components/LeaderboardRow.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export class LeaderboardRow extends Component {
+    render() {
+        const { user } = this.props
+        console.log(this.props)
+        return (
+            <div className="row m-3 border border-secondary rounded">
+                <div className="col-3 p-2 border-end border-2 border-secondary">
+                    <div className="pos">
+                        #{user.pos}
+                    </div>
+                    <img
+                        src={user.avatarURL}
+                        alt="avatar"
+                        className="large-avatar-image m-auto"
+                    />
+                </div>
+                <div className="col-6 lb-row-mid border-end border-2 border-secondary">
+                    <div className="fw-bold h5 m-0 mt-1">{user.name}</div>
+                    <div className="row m-0">
+                        <div className="col-8 m-0 p-0 text-start">
+                            Asked:
+                        </div>
+                        <div className="col-4 m-0 ps-2 text-start">
+                            {user.asked}
+                        </div>
+                    </div>
+                    <div className="row m-0">
+                        <div className="col-8 m-0 p-0 text-start">
+                            Answered:
+                        </div>
+                        <div className="col-4 m-0 ps-2 text-start">
+                            {user.answered}
+                        </div>
+                    </div>
+                </div>
+                <div className="col-3 lb-row-end">
+                    <div className="fw-bold border rounded">
+                        <div className="border rounded-top">
+                            Points
+                        </div>
+                        <div className="p-2">
+                            <div className="rounded-circle m-auto square pts">
+                                {user.total}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LeaderboardRow
