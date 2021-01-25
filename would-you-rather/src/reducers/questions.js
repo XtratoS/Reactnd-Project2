4e70e52e891acc0ad4926034198716d35d76d4dd
XtratoS <ultimate.nuker@yahoo.com>
2021-01-25 23:55:39 +0200

    Handling ADD_ANSWER in both questions' and users' portions of the state

--- would-you-rather/src/reducers/questions.js
+++ would-you-rather/src/reducers/questions.js
@@ -1,4 +1,5 @@
 import { RECEIVE_QUESTIONS, ADD_QUESTION } from "../actions/questions";
+import { ADD_ANSWER } from "../actions/shared";
 
 export function questions(state = {}, action) {
     switch(action.type) {
@@ -6,6 +7,19 @@ export function questions(state = {}, action) {
             return action.questions
         case ADD_QUESTION:
             return {...state, [action.question.id]: {...action.question}}
+        case ADD_ANSWER:
+            console.log(action);
+            const { questionId, userId, answer } = action.answer;
+            return {
+                ...state,
+                [questionId]: {
+                    ...state[questionId],
+                    [answer]: {
+                        ...state[questionId][answer],
+                        votes: state[questionId][answer].votes.concat([userId])
+                    }
+                }
+            }
         default:
             return state
     }
--- would-you-rather/src/reducers/users.js
+++ would-you-rather/src/reducers/users.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USERS, ADD_USER } from "../actions/users";
+import { ADD_ANSWER } from "../actions/shared";
 
 export function users(state = {}, action) {
     switch(action.type) {
@@ -6,6 +7,18 @@ export function users(state = {}, action) {
             return action.users;
         case ADD_USER:
             return {...state, ...action.user}
+        case ADD_ANSWER:
+            const { userId, questionId, answer } = action.answer
+            return {
+                ...state,
+                [userId]: {
+                    ...state[userId],
+                    answers: {
+                        ...state[userId].answers,
+                        [questionId]: answer
+                    }
+                }
+            }
         default:
             return state;
     }
