6e7b9e5124db40c45079fb94e199c858fe363c35
XtratoS <ultimate.nuker@yahoo.com>
2021-01-28 04:28:12 +0200

    When creating a new question, concatenate its id to the questions array of the appropriate user in the users' portion of the state

--- would-you-rather/src/reducers/users.js
+++ would-you-rather/src/reducers/users.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USERS, ADD_USER } from "../actions/users";
 import { ADD_ANSWER } from "../actions/shared";
+import { ADD_QUESTION } from "../actions/questions";
 
 export function users(state = {}, action) {
     switch(action.type) {
@@ -7,6 +8,15 @@ export function users(state = {}, action) {
             return action.users;
         case ADD_USER:
             return {...state, ...action.user}
+        case ADD_QUESTION:
+            const { author, id } = action.question;
+            return {
+                ...state,
+                [author]: {
+                    ...state[author],
+                    questions: state[author].questions.concat([id])
+                }
+        }
         case ADD_ANSWER:
             const { userId, questionId, answer } = action.answer
             return {
